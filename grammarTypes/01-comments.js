c54049204571bc0b343d586f535396e9182ae24a
ashtonsprunger <sprungerashton@gmail.com>
2020-09-10 15:14:52 -0400

    first push

---
+++ grammarTypes/01-comments.js
@@ -0,0 +1,13 @@
+// single line comment
+
+// quick note
+
+// here is my quick note
+
+/* 
+    !sldkfj
+    ?s;dlkfj
+    sldfkja
+    sdafdsssssssssssssssssssssssssss
+    dfds
+*/
---
+++ grammarTypes/02-declarations.js
@@ -0,0 +1,12 @@
+/*
+! variables
+*/
+
+console.log('hello');
+
+
+let a = 2;
+
+let b = 1;
+
+console.log(a + b);
