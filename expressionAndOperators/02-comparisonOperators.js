0236e6f2ae10dc1957e29fe712135c61fdba4c65
ashtonsprunger <sprungerashton@gmail.com>
2020-09-11 15:17:30 -0400

    update 9/11

---
+++ expressionAndOperators/02-comparisonOperators.js
@@ -0,0 +1,11 @@
+function addNums(numOne, numTwo=1) {
+    return numOne + numTwo;
+}
+
+console.log(addNums(4))
+console.log(addNums(4, 3))
+
+console.log(5 && 4);
+if('this' && 'that') {
+    console.log('yes')
+}
---
+++ grammarTypes/03-types.js
@@ -0,0 +1,23 @@
+/*
+    Set 7 (or 8) variables:
+    firstName
+    lastName
+    houseNumber
+    aptNumber (if applicable)
+    street
+    city
+    state
+    zipcode
+    Set each variable to its respective type.
+    console.log your whole address (with a space between variables).
+*/
+
+const firstName = 'Ashton';
+const lastName = 'Sprunger';
+let houseNumber = 11323;
+let street = 'Canter CT.';
+let city = 'Fort Wayne';
+let state = 'Indiana';
+let zipcode = 46845;
+
+console.log(firstName, lastName + '\n' + houseNumber, street, '\n' + city, state, zipcode);
